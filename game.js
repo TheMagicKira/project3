cafd74ffdfa4ab0f0abc3ef1aa6bd15031a515a8
Daniel Perrone <113133556+TheMagicKira@users.noreply.github.com>
2022-11-03 22:54:02 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,3 @@
-    var sec = 0;
-    function pad ( val ) { return val > 9 ? val : "0" + val; }
-    setInterval( function(){
-        document.getElementById("seconds").innerHTML=pad(++sec%60);
-        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-    }, 1000);
-
-<span id="minutes"></span>:<span id="seconds"></span>
-    
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
